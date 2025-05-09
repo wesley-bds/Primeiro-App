307aca26cacab7ca6a94e47d0eea24cad34646d9
Wesley <wesleybarbosads.98@gmail.com>
2025-05-09 00:10:01 -0300

    pasta services

---
+++ services/dattebayo-api/personagens/list.ts
@@ -0,0 +1,18 @@
+const API_HOST = 'https://dattebayo-api.onrender.com'
+
+export async function fetchPersonagens(){
+    try {
+        const response = await fetch(`${API_HOST}/characters`)
+
+        if (!response.ok){
+            throw new Error(`Erro na requisição: ${response.statusText}`)
+        }
+        return await response.json();
+
+    } catch (error) {
+        console.error(`Erro ao buscar personagens: ${error}`)
+        return [];
+    }
+
+}
+
